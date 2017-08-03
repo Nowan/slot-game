f9ba3d8b399200b2892c13d027dade71cf6a1904
dnipromv <dnipromv@gmail.com>
2017-08-03 14:43:38 +0200

    image preloading

--- src/states/Load.js
+++ src/states/Load.js
@@ -12,14 +12,32 @@ function LoadState(){
     });
   }
   
-  this.startLoading = function(loadCallback){
-    _fetchResources()
-      .then((resources) => {
-        console.log(resources);
-      })
-      .catch((errorMessage) => {
-        console.log(errorMessage);
+  function _loadImage(resource){
+    return new Promise((resolve, reject) => {
+      var image = new Image();
+      image.onload = () => resolve({ resource: resource, image: image });
+      image.src = resource.path;
+    });
+  }
+  
+  this.loadImages = function(){
+    return new Promise((resolve, reject) => {
+      _fetchResources().then((resources) => {
+        var loaded_images = { length: 0 };
+        
+        function onImageLoad(data){
+          loaded_images.length++;
+          loaded_images[data.resource.id] = data.image;
+          if(loaded_images.length >= resources.length)
+            resolve(loaded_images);
+        }
+      
+        for( var i = 0; i < resources.length; i++ ){
+          var resource = resources[i];
+          _loadImage(resource).then(onImageLoad);
+        }
       });
+    });
   }
   
 }
